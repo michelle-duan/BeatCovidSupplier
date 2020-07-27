4b585b8959831801ca7ce56e19f7031f6c124ff0
michelled <michelleddevop@gmail.com>
2020-07-27 14:52:06 -0700

    Username as Firebase key

---
+++ app/src/main/java/com/michelleduan/beatcovidsupplier/Prevalent/Prevalent.java
@@ -0,0 +1,7 @@
+package com.michelleduan.beatcovidsupplier.Prevalent;
+
+import com.michelleduan.beatcovidsupplier.Model.Users;
+
+public class Prevalent {
+    private static Users currentOnlineUser;
+}
