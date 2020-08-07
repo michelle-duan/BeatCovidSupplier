0360242223d6a514367b386bff07ef707130f677
Shubham Singal <shubhamrsingal@gmail.com>
2020-08-07 12:33:40 -0400

    Viewing Individual Product

--- app/src/main/java/com/michelleduan/beatcovidsupplier/ProductDetailsActivity.java
+++ app/src/main/java/com/michelleduan/beatcovidsupplier/ProductDetailsActivity.java
@@ -8,24 +8,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.michelleduan.beatcovidsupplier.Model.Products;
+import com.squareup.picasso.Picasso;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
-    private FloatingActionButton addToCartBtn;
+//    private FloatingActionButton addToCartBtn;
     private ImageView productImage;
     private ElegantNumberButton numberButton;
     private TextView productPrice, productDescription, productName;
+    private String productID = "";
 
-
-    protected void OnCreate(Bundle savedInstanceState){
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
 
-        addToCartBtn = findViewById(R.id.add_product_to_cart_btn);
+        productID = getIntent().getStringExtra("pid");
+
+//        addToCartBtn = findViewById(R.id.add_product_to_cart_btn);
         numberButton = (ElegantNumberButton) findViewById(R.id.number_btn);
         productImage = (ImageView) findViewById(R.id.product_image_details);
         productName = (TextView) findViewById(R.id.product_name_details);
         productDescription = (TextView) findViewById(R.id.product_description_details);
         productPrice = (TextView) findViewById(R.id.product_price_details);
+
+        getProductDetails(productID);
+    }
+
+    private void getProductDetails(String productID) {
+        DatabaseReference productsRef = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        productsRef.child(productID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.exists())
+                {
+                    Products products = dataSnapshot.getValue(Products.class);
+
+                    productName.setText(products.getPname());
+                    productPrice.setText(products.getPrice());
+                    productDescription.setText(products.getDescription());
+                    Picasso.get().load(products.getImage()).into(productImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
