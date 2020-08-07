34438aa08971ff492981131f13a646a44cbf082c
Shubham Singal <shubhamrsingal@gmail.com>
2020-08-07 15:30:36 -0400

    Adding Admin Logout functionality

--- app/src/main/java/com/michelleduan/beatcovidsupplier/AdminCategoryActivity.java
+++ app/src/main/java/com/michelleduan/beatcovidsupplier/AdminCategoryActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class AdminCategoryActivity extends AppCompatActivity {
 
     private ImageView facemask, gloves, thermometer, glasses,isolationmask, others;
+
+    private Button LogoutBtn, CheckOrdersBtn;
 
 
     @Override
@@ -23,6 +26,9 @@ public class AdminCategoryActivity extends AppCompatActivity {
         glasses = (ImageView)findViewById(R.id
                 .glasses);
         isolationmask = (ImageView)findViewById(R.id.isolationmask);
+        others = (ImageView)findViewById(R.id.otherproducts);
+        LogoutBtn = (Button) findViewById(R.id.admin_logout_btn);
+        CheckOrdersBtn = (Button) findViewById(R.id.check_orders_btn);
         others = (ImageView)findViewById(R.id.otherproducts);
 
         facemask.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +81,26 @@ public class AdminCategoryActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
                 intent.putExtra("category","Other Products");
+                startActivity(intent);
+            }
+        });
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminNewOrdersActivity.class);
                 startActivity(intent);
             }
         });
