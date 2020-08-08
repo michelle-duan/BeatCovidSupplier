dcfebd422bf81b6b50d669ff2613d87d6e4be3c5
michelled <michelleddevop@gmail.com>
2020-08-07 23:15:06 -0700

    video 27 tested

--- app/src/main/java/com/michelleduan/beatcovidsupplier/ConfirmFinalOrderActivity.java
+++ app/src/main/java/com/michelleduan/beatcovidsupplier/ConfirmFinalOrderActivity.java
@@ -33,7 +33,7 @@ public class ConfirmFinalOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_confirm_final_order);
 
         totalAmount = getIntent().getStringExtra("Total Price");
-        Toast.makeText(this, "Total Price = $" + totalAmount, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Total Price: $" + totalAmount, Toast.LENGTH_SHORT).show();
 
         confirmOrderBtn = (Button) findViewById(R.id.confirm_final_order_btn);
         nameEditText = (EditText) findViewById(R.id.shippment_name);
