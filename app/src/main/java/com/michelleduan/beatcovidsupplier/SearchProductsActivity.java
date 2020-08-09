53aba1db74cfe79f081c3796b574779475054a7c
michelled <michelleddevop@gmail.com>
2020-08-08 20:17:30 -0700

    8.13 Testing

--- app/src/main/java/com/michelleduan/beatcovidsupplier/SearchProductsActivity.java
+++ app/src/main/java/com/michelleduan/beatcovidsupplier/SearchProductsActivity.java
@@ -1,10 +1,5 @@
 package com.michelleduan.beatcovidsupplier;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,6 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
@@ -21,8 +21,7 @@ import com.michelleduan.beatcovidsupplier.Model.Products;
 import com.michelleduan.beatcovidsupplier.ViewHolder.ProductViewHolder;
 import com.squareup.picasso.Picasso;
 
-public class SearchProductsActivity extends AppCompatActivity {
-
+public class SearchProductsActivity extends AppCompatActivity{
     private Button SearchBtn;
     private EditText inputText;
     private RecyclerView searchList;
@@ -30,6 +29,7 @@ public class SearchProductsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_products);
 
@@ -38,29 +38,27 @@ public class SearchProductsActivity extends AppCompatActivity {
         searchList = findViewById(R.id.search_list);
         searchList.setLayoutManager(new LinearLayoutManager(SearchProductsActivity.this));
 
-        SearchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
+        SearchBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
                 SearchInput = inputText.getText().toString();
                 onStart();
             }
         });
-
     }
 
     @Override
-    protected void onStart(){
-
+    protected void onStart() {
 
         super.onStart();
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
 
-
-        FirebaseRecyclerOptions<Products> options =
-                new FirebaseRecyclerOptions.Builder<Products>()
-                .setQuery(reference.orderByChild("pname").startAt(SearchInput),Products.class).build();
+        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(reference.orderByChild("category"), Products.class)
+                .build();
 
         FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
                 new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
@@ -69,29 +67,32 @@ public class SearchProductsActivity extends AppCompatActivity {
 
                         holder.txtProductName.setText(model.getPname());
                         holder.txtProductDescription.setText(model.getDescription());
-                        holder.txtProductPrice.setText("$" + model.getPrice());
+                        holder.txtProductPrice.setText("Price: $" + model.getPrice());
                         Picasso.get().load(model.getImage()).into(holder.imageView);
 
                         holder.itemView.setOnClickListener(new View.OnClickListener() {
                             @Override
-                            public void onClick(View view) {
+                            public void onClick(View v) {
                                 Intent intent = new Intent(SearchProductsActivity.this, ProductDetailsActivity.class);
                                 intent.putExtra("pid", model.getPid());
                                 startActivity(intent);
                             }
                         });
+
                     }
 
                     @NonNull
                     @Override
                     public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent, false);
+
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent,false);
                         ProductViewHolder holder = new ProductViewHolder(view);
                         return holder;
+
                     }
                 };
-
         searchList.setAdapter(adapter);
         adapter.startListening();
     }
+
 }
